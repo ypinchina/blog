de185e578c65814c30e162faa024c79362e9f229
YipenG <batmanyp@foxmail.com>
2016-12-06 15:52:07 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,36 +4,38 @@
 // 入口
 $(document).ready(function () {
     showTopLogo();
-
-
-
-})
-
-
-
-function showTopLogo() {
-    var topButtom = $(".slice_bar .top");
-
-    var right_info = $('.person_info');//右方个人信息
     sidebar();
+    })
+
+var topButtom = $(".slice_bar .top");
+var right_info = $('.person_info');
+
+function sidebar() {
     var isSidebarVisible;
     var SIDEBAR_WIDTH = 570;
+    right_info.on('click', function () {
+        var sidebarWidth = isSidebarVisible ? 0 : SIDEBAR_WIDTH;
+
+        $('.about_me').animate({
+            width: sidebarWidth
+        }, 800);
+        $('.center_container').animate({
+            paddingRight: sidebarWidth-230
+        }, 800);
+
+        isSidebarVisible = !isSidebarVisible;
+    });
+}
+function showTopLogo() {
+
+
+
 
 
     topButtom.click(function () {
         $("html,body").animate({scrollTop: 0}, 1000);//点击回到顶部
     })
 
-    function sidebar() {
-        right_info.on('click', function () {
-            var sidebarWidth = isSidebarVisible ? 0 : SIDEBAR_WIDTH;
 
-            $('.about_me').animate({
-                width: sidebarWidth
-            }, 800);
-
-            isSidebarVisible = !isSidebarVisible;
-        });
-    }
 
 }

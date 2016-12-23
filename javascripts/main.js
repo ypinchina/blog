8db285bf4ef8aa5b52699a4de137428845be059f
PengYi <batmanyp@foxmail.com>
2016-12-23 15:03:41 +0800

    加入了站长统计居中类

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,14 @@
 /**
- * Created by Administrator on 2016/12/5.
+ * Created by Aoyama Nanami on 2016/12/5.
  */
 // 入口
 $(document).ready(function () {
 
     showTopLogo();
     sidebar();
+    cnzzLoad();
 
-    })
+})
 
 var topButtom = $(".slice_bar .top");
 var right_info = $('.person_info');
@@ -32,20 +33,28 @@ function sidebar() {
 function showTopLogo() {
     topButtom.fadeOut();
     $(window).scroll(function () {
-        if($(document).scrollTop()>400){
+        if ($(document).scrollTop() > 400) {
             topButtom.fadeIn();
-        }else {
+        } else {
             topButtom.fadeOut()
         }
     });
     topButtom.click(function () {
-        if($(document).scrollTop()==0){
+        if ($(document).scrollTop() == 0) {
+
             return null;
         }//解决多次点击top的BUG
 
-       else{$("html,body").animate({scrollTop: 0}, 1000);} //点击回到顶部
+        else {
+            $("html,body").animate({scrollTop: 0}, 1000);
+        } //点击回到顶部
     })
 
 
+}
+function cnzzLoad() {
+    var cnzz = $("#cnzz_stat_icon_1261034251");
+    cnzz.wrap("<div style=\'width: 100%;background-color: #333;\'></div>");
+    cnzz.addClass("center_container");
 
 }
